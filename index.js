74e584be01dfefb1e430345185562a286fb1114d
Amanda <mandimush@gmail.com>
2022-07-20 20:56:11 -0600

    js index added

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//Che Shumacher 2022 ease in ease out accorian
+.toggleContainer {
+    /* padding: 5px; */
+    /* display: none; */
+    overflow: hidden;
+    max-height: 0px;
+    transition: all .7s ease;
+  }
+  
+  .active {
+    /* display: block; */
+    max-height: 200px;
+  }
